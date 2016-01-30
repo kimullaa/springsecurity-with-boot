b18cf17828a1423773e100f4b49e0171dcc2a08f
kimullamen <pbrehpuum@gmail.com>
2016-01-30 13:31:42 +0900

    first commit

---
+++ src/main/java/com/example/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/admin").setViewName("admin/home");
+		registry.addViewController("/admin/home").setViewName("admin/home");
+	}
+
+}
